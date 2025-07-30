461181d4c020561d0226cb87fb214fa591838152
Serianth <222801879+Serianth@users.noreply.github.com>
2025-07-30 17:03:50 +0100

    Grouped updatables and drawables, included player movement

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
-
+    
     while True:
         # Event handling (makes close button work)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
+        updatable.update(dt)
         # Fill screen with black
         screen.fill("black")
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         # Refresh the display
         pygame.display.flip()
         dt = clock.tick(60) / 1000
